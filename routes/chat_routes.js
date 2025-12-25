0725eba9c389efdae71ab92197b257a6857bd1fc
hollanderd6-code <hollanderd6@gmail.com>
2025-12-25 14:48:51 +0100

    Update chat_routes.js

--- routes/chat_routes.js
+++ routes/chat_routes.js
@@ -88,10 +88,10 @@ function generateAutoResponse(property, detectedQuestions) {
     
     switch (question.category) {
       case 'checkin':
-        if (property.arrival_time) response = \`L'arrivée est possible à partir de \${property.arrival_time}.\`;
+        if (property.arrival_time) response = `L'arrivée est possible à partir de ${property.arrival_time}.`;
         break;
       case 'checkout':
-        if (property.departure_time) response = \`Le départ doit se faire avant \${property.departure_time}.\`;
+        if (property.departure_time) response = `Le départ doit se faire avant ${property.departure_time}.`;
         break;
       case 'draps':
         response = amenities.draps ? 'Oui, les draps sont fournis.' : 'Non, les draps ne sont pas fournis.';
@@ -104,18 +104,18 @@ function generateAutoResponse(property, detectedQuestions) {
         break;
       case 'wifi':
         if (property.wifi_name && property.wifi_password) {
-          response = \`Réseau WiFi : "\${property.wifi_name}"\nMot de passe : "\${property.wifi_password}"\`;
+          response = `Réseau WiFi : "${property.wifi_name}"\nMot de passe : "${property.wifi_password}"`;
         }
         break;
       case 'acces_code':
-        if (property.access_code) response = \`Le code d'accès est : \${property.access_code}\`;
+        if (property.access_code) response = `Le code d'accès est : ${property.access_code}`;
         break;
       case 'animaux':
         response = houseRules.animaux ? 'Oui, les animaux sont acceptés.' : 'Non, les animaux ne sont pas acceptés.';
         break;
       case 'parking':
         if (amenities.parking && practicalInfo.parking_details) {
-          response = \`Oui, voici les informations parking : \${practicalInfo.parking_details}\`;
+          response = `Oui, voici les informations parking : ${practicalInfo.parking_details}`;
         } else if (amenities.parking) {
           response = 'Oui, un parking est disponible.';
         }
